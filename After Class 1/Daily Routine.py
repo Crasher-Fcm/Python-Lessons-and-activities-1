2c9dd979e3370edc3cff606dac16265c866c60f0
Crasher-Fcm <leslietetteh31@gmail.com>
2026-05-03 17:02:02 +0000

    My Daily Routine

---
+++ After Class 1/Daily Routine.py
@@ -0,0 +1,14 @@
+# print the title
+print("---My Daily Routine---")
+
+# print the daily routine
+print("I wake up at 5:30 am.")  
+print("I have breakfast at 6:00 am.")
+print("I go to school at 6:30 am.")
+print("I have lunch at 12:00 pm.")
+print("I finish school at 3:00 pm.")
+print("I play with my friends at 4:00 pm.")
+print("I have dinner at 7:00 pm.")
+print("I do my homework at 8:00 pm.")
+print("I study Python at 8:30 pm.")
+print("I go to bed at 10:00 pm.")
