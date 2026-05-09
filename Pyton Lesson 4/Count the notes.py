abbea7d26aac1345770fcb47c3e3d19daa3614db
Crasher-Fcm <leslietetteh31@gmail.com>
2026-05-09 16:56:55 +0000

    Count the notes

---
+++ Pyton Lesson 4/Count the notes.py
@@ -0,0 +1,10 @@
+# Taking total as input from user
+Amount = int(input("Please enter amount for withdrawal : "))    
+
+# Calculating the number of notesof different denominations
+note_1 = Amount // 100
+note_2 = (Amount % 100) // 50
+note_3 = ((Amount % 100) % 50) // 5
+print("notes of 100 cedis" , note_1)
+print("notes of 70 cedis" , note_2)
+print("notes of 5 cedis" , note_3)
