78f98cdc9691a31cfc26d9a587ce4fd47738f84e
Crasher-Fcm <leslietetteh31@gmail.com>
2026-05-10 17:21:36 +0000

    Summertimes

---
+++ After class 5/Summer times.py
@@ -0,0 +1,8 @@
+temperature = float(input("Enter the temperature in Celsius: "))
+
+if temperature > 30:
+    print("It's a hot summer day! Grab some ice cream.")
+elif temperature > 20:
+    print("The weather is perfect for a swim.")
+else:
+    print("It's a bit cool for summer , maybe stay inside.")
