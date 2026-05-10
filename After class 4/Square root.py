4cd8b108d953d801c7cd4486b81df64b7794aa36
Crasher-Fcm <leslietetteh31@gmail.com>
2026-05-10 17:12:08 +0000

    Square root

---
+++ After class 4/Square root.py
@@ -0,0 +1,8 @@
+# input number
+number = float(input("Finding the square root of: "))
+
+# calculate square root
+square_root = number ** 0.5
+
+# display result
+print("The square root of", number, "is", square_root)
