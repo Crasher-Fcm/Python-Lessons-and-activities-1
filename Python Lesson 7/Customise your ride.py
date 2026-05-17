19291c4b876b9e1777aba7ded439d54193d1a094
Crasher-Fcm <leslietetteh31@gmail.com>
2026-05-17 17:51:50 +0000

    Customise your ride

--- Python Lesson 7/Customise your ride.py
+++ Python Lesson 7/Customise your ride.py
@@ -1,35 +1,43 @@
-    print("Select your type of ride")
-    print("1.  Bike")
-    print("2.  Car")
+print("Select your ride")
+print("1. Bike")
+print("2. Car")
+print("3. Bus")
 
-    choice = int(input("Enter your choice: "))
+choice = int(input("Enter your choice: "))
 
-    if choice == 1:
-        print("What type of bike?")
-        print("1. Sports Bike\n")
-        print("2. Cruiser Bike\n")
+if choice == 1:
+    print("What type of bike?")
+    print("1. Scooty\n")
+    print("2. Scooter\n")   
 
-        choice_2 = int(input("Enter your choice"))
-        if choice_2 == 1:
-           print("You have selected Sports Bike")
-        else:
-           print("You have selected Cruiser Bike") 
-
-    elif (choice == 2):   
-
-        print("What type of car?")   
-        print("1. Mercedes\n") 
-        print("2. Sedan\n")
-
-        choice_3 = int(input("Enter your choice"))
-        if choice_3 == 1:
-            print("You have selected Mercedes")
-        else:
-            print("You have selected Sedan")
+    choice_2 = int(input("Enter your choice: "))
+    if choice_2 == 1:
+        print("You have selected Scooty")
     else:
-        print("Wrong Choice")    
+        print("You have selected Scooter")
 
+elif (choice ==2):
 
+    print("What type of car?")
+    print("1. Sedan\n")
+    print("2. Hatchback\n")
 
+    choice_3 = int(input("Enter your choice: "))
+    if choice_3 == 1:
+        print("You have selected Sedan")
+    else:
+        print("You have selected Hatchback")
 
-       
+elif (choice ==3):
+
+    print("What type of bus?")
+    print("1. Scania\n")
+    print("2. Taxi\n")
+
+    choice_4 = int(input("Enter your choice: "))
+    if choice_4 == 1:
+        print("You have selected Scania")
+    else:
+        print("You have selected Taxi")
+else:
+    print("Wrong Choice")
