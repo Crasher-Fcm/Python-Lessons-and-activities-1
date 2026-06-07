1c9d9492b8554644bcfe9e871c04aa9631c0be40
Crasher-Fcm <leslietetteh31@gmail.com>
2026-06-07 16:53:32 +0000

    Calculator

---
+++ Python Lesson 13/Weather condition.py
@@ -0,0 +1,8 @@
+def weather_condition():
+    print('The weather is enjoyable in:', autumn)
+    print('The weather is alsoenjoyable in:', spring)
+
+
+autumn = "Autumn"
+spring = "Spring"
+weather_condition()
