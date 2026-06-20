8556a3f6e717367a98c0818cc311ca6b755aa869
Crasher-Fcm <leslietetteh31@gmail.com>
2026-06-20 16:59:12 +0000

    Keywords in python

---
+++ Python Lesson 15/Break.py
@@ -0,0 +1,10 @@
+a = input ("Enter a word: ")
+
+for i in a:
+    if (i == 'a'):
+
+      print ("The letter A is present in the word")
+      break 
+    else:
+        print ("The letter A is not present in the word")
+         
---
+++ Python Lesson 15/Pass.py
@@ -0,0 +1,15 @@
+for x in range(10):
+    if x % 20 ==0:
+        print("twister")
+
+    elif x % 15 == 0:
+        pass
+
+    elif x % 5 == 0:
+        print("buckle")
+
+    elif x % 3 == 0:
+        print("my shoe")
+
+    else:
+        print(x)     
